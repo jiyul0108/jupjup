23eaa40a187c4743b0baa964971fa9e38e9e609e
jiyul <rlawldbf0108@gmail.com>
2026-06-15 18:26:51 +0900

    fix: CORS 설정 추가

--- src/main/java/com/jupjup/Backend/global/config/WebConfig.java
+++ src/main/java/com/jupjup/Backend/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.jupjup.Backend.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Value("${file.upload-dir}")
     private String uploadDir;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
